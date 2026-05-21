fbed02357022ff7ac150f3cc21d4268a5484821a
lukhangelelandu-crypto <lukhangele.landu@gmail.com>
2026-05-21 15:20:26 +0200

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,21 @@
+// TODO: Create an empty array called colors 
+let colors = []
+
+// TODO: Add three colors to the array using push()
+colors.push("red")
+colors.push("black")
+colors.push("white")
+
+// TODO: Create another array called numbers with five numbers
+let numbers = [2, 4, 6, 8, 10]
+
+// TODO: Remove the last color from the colors array
+colors.pop()
+
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift("blue")
+
+// TODO: Print the length of both arrays
+console.log("Length of colors array:", colors.length)
+console.log("Length of numbers array:", numbers.length)
+
